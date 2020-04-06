541d38e6c1d253a7d4c020c81353780a0c00d49e
Gina Frankel <g.frankel22@gmail.com>
2020-04-06 15:37:03 +0100

    feature test - showing bookmarks: clicking 'View bookmarks' button should take you to page '/bookmarks - passing

--- spec/features/showing_bookmarkes_spec.rb
+++ spec/features/showing_bookmarkes_spec.rb
@@ -3,4 +3,11 @@ feature "showing bookmarks" do
     visit('/')
     expect(page).to have_button('View Bookmarks')
   end
+  scenario "clicking 'View bookmarks' button should take you to page '/bookmarks" do
+    visit('/')
+    click_button 'View Bookmarks'
+    expect(page).to have_current_path('/bookmarks')
+  end 
+
+
 end
