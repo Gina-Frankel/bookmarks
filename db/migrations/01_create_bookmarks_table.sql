9f8be349cdb4d7059b8a788ff965c072df1699f7
tommyrharper <tommyrharper@gmail.com>
2020-04-08 15:42:22 +0100

    test database installed

--- db/migrations/01_create_bookmarks_table.sql
+++ db/migrations/01_create_bookmarks_table.sql
@@ -1,3 +1,8 @@
 CREATE TABLE bookmarks(id SERIAL PRIMARY KEY, url VARCHAR(60));
 
  Update link to http://www.destroyallsoftware.com using an UPDATE statement.
+
+
+user=# CREATE TABLE bookmarks(id SERIAL PRIMARY KEY, url VARCHAR(60));
+user=# \c bookmark_manager_test;
+bookmark_manager_test=# CREATE TABLE bookmarks(id SERIAL PRIMARY KEY, url VARCHAR(60));
